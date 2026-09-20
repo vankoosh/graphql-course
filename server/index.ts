2d49bba785a1010a8e23678309fc93b4afc2b07c
Mario Nano <nano@nanoV15.localdomain>
2026-09-20 10:16:45 +0200

    basic setup done

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,43 @@
+import "dotenv/config";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from '@as-integrations/express5';
+import cors from "cors";
 import express from "express";
+import cookieParser from "cookie-parser";
 
-const app = express();
+const CLIENT_PORT = Number(process.env.CLIENT_LOCALHOST_PORT) || '';
+const SERVER_PORT = Number(process.env.SERVER_LOCALHOST_PORT) || '';
+
+const
+  app = express();
+
+const server = new ApolloServer({
+  typeDefs: `
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: { hello: () => "Hello World" }
+  }
+})
 
 async function startServer() {
-  app.listen(4000, () => {
-    console.log("Server is running on http://localhost:4000/graphql");
+  await server.start();
+  app.use(
+    "/graphql",
+    cors({
+      origin: `http://localhost:${CLIENT_PORT}`,
+      credentials: true
+    }),
+    cookieParser(),
+    express.json(), // graphql will always respond in JSON
+    expressMiddleware(server)
+  )
+
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on http://localhost:${SERVER_PORT}/graphql`);
   });
 }
 
-startServer();
+startServer().then();
